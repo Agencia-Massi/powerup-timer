7f9eb5c6d32bae49cb7049a241632a9264648842
miguelnsimoes <miguelnazariosimoes@gmail.com>
2025-12-02 14:52:29 -0300

    commit 2

--- js/client.js
+++ js/client.js
@@ -1,15 +1,16 @@
-TrelloPowerUp.inicialize({
+
+
+TrelloPowerUp.initialize({ 
     'card-buttons': function(t, options){
         return[{
             icon: 'https://cdn.icon-icons.com/icons2/1369/PNG/512/-timer_90562.png',
             text: 'Timer teste',
             callback: function(t){
                 return t.alert({
-                    message: 'o powerup ta ativo',
+                    message: 'O powerup está ativo!',
                     duration: 5,
                 })
             }
         }]
     }
 })
-
