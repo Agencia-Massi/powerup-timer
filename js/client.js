795459e3de98b2e85b120e50bd3e71457c6ac147
miguelnsimoes <miguelnazariosimoes@gmail.com>
2025-12-02 15:45:48 -0300

    a

--- js/client.js
+++ js/client.js
@@ -1,16 +1,19 @@
+// NOVO CÓDIGO para o seu js/client.js
+const GITHUB_PAGES_BASE = 'https://miguelnsimoes.github.io/meu-trello-timer';
 
-
-TrelloPowerUp.initialize({ 
-    'card-buttons': function(t, options){
-        return[{
-            icon: 'https://cdn.icon-icons.com/icons2/1369/PNG/512/-timer_90562.png',
+TrelloPowerUp.initialize({
+    'card-buttons': function(t, options) {
+        return [{
+            // Aponta para o arquivo SVG no seu próprio GitHub Pages
+            icon: `${GITHUB_PAGES_BASE}/img/icon.svg`, 
             text: 'Timer teste',
-            callback: function(t){
+            callback: function(t) {
                 return t.alert({
                     message: 'O powerup está ativo!',
                     duration: 5,
-                })
+                });
             }
-        }]
-    }
-})
+        }];
+    },
+    // ... outras capabilities
+});
